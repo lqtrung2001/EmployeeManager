46c3a2fc93c45c99db718dad6bece3cd40ca32b8
Luong Quoc Trung <lqtrung20122001@gmail.com>
2022-10-07 00:46:28 +0700

    FE delete and search

--- EmployeeManager-front/src/app/app.component.ts
+++ EmployeeManager-front/src/app/app.component.ts
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
   // @ts-ignore
   public editEmployee : Employee;
   // @ts-ignore
-  private deleteEmployee: Employee;
+  public deleteEmployee: Employee;
 
   constructor(private employeeService: EmployeeService) {
   }
@@ -39,7 +40,7 @@ export class AppComponent implements OnInit {
       }
     );
   }
-  public onUpdateAddEmployee(employee: Employee): void {
+  public onUpdateEmployee(employee: Employee): void {
     this.employeeService.updateEmployee(employee).subscribe(
       (response: Employee) => {
         console.log(response);
@@ -50,6 +51,17 @@ export class AppComponent implements OnInit {
       }
     );
   }
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+   }
   public getEmployees(): void {
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => {
@@ -61,7 +73,22 @@ export class AppComponent implements OnInit {
       }
     );
   }
-
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
   public onOpenModal(employee: Employee | null, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
